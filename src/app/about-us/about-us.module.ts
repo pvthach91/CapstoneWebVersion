5f3d356dfc80b29ec5057e1f89b8a09e25fbc173
Thach <pvthach91@gmail.com>
2020-04-05 17:00:28 +0800

    update

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -7,15 +7,13 @@ import { IonicModule } from '@ionic/angular';
 import { AboutUsPageRoutingModule } from './about-us-routing.module';
 
 import { AboutUsPage } from './about-us.page';
-import {FooterPageModule} from "../footer/footer.module";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
-        AboutUsPageRoutingModule,
-        FooterPageModule
+        AboutUsPageRoutingModule
     ],
   declarations: [AboutUsPage]
 })
