fc8e32ac9abfbadb59c037ef62cef0f272d17fce
Thach <pvthach91@gmail.com>
2020-05-04 17:08:21 +0800

    update

--- src/app/my-account/order/order-view/order-view.page.ts
+++ src/app/my-account/order/order-view/order-view.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from "../../../model/order.model";
+import {ActivatedRoute} from "@angular/router";
+import {OrderService} from "../../../services/order.service";
+import {AlertController} from "@ionic/angular";
+import {configuration} from "../../../model/configuration.model";
+import {User} from "../../../model/user.model";
 
 @Component({
   selector: 'app-order-view',
@@ -7,9 +13,159 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderViewPage implements OnInit {
 
-  constructor() { }
+  id;
+  order: Order = new Order(null, '','', new User(), '', 0,0, 0,'','', 0, []);
+  isProcessing: boolean = false;
+  isProcessed: boolean = false;
+  isDelivering: boolean = false;
+  isFinished: boolean = false;
+  isCancelled: boolean = false;
+
+  configuration = configuration;
+
+  constructor(private route: ActivatedRoute,
+              private orderService: OrderService,
+              public alertController: AlertController) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      if (this.id == null || this.id == undefined) {
+        // Load new page
+      } else {
+        // Load detail page
+        this.getCurrentOrder();
+      }
+    });
+
+  }
+
+  async presentAlert(header: string, subHeader: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      subHeader: subHeader,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  getCurrentOrder() {
+    this.orderService.getOrder(this.id).subscribe(
+        data => {
+          if (data.success) {
+            this.order = data.data;
+            this.setStatuses();
+          } else {
+
+          }
+        },
+        error => {
+          console.log(error);
+        }
+    );
+  }
+
+  setStatuses(){
+    if (this.order.status == 'PROCESSING') {
+      this.isProcessing = true;
+    } else {
+      this.isProcessing = false;
+    }
+
+    if (this.order.status == 'PROCESSED') {
+      this.isProcessed = true;
+    } else {
+      this.isProcessed = false;
+    }
+
+    if (this.order.status == 'DELIVERING') {
+      this.isDelivering = true;
+    } else {
+      this.isDelivering = false;
+    }
+
+    if (this.order.status == 'FINISHED') {
+      this.isFinished = true;
+    } else {
+      this.isFinished = false;
+    }
+
+    if (this.order.status == 'CANCELLED') {
+      this.isCancelled = true;
+    } else {
+      this.isCancelled = false;
+    }
+  }
+
+  processOrder(orderId: number) {
+    this.orderService.process(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Processed successfully');
+            this.order = data.data;
+            this.setStatuses();
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to process the order');
+        }
+    );
+  }
+
+  deliverOrder(orderId: number) {
+    this.orderService.deliver(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Delivering successfully');
+            this.order = data.data;
+            this.setStatuses();
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to deliver the order');
+        }
+    );
+  }
+
+  finishOrder(orderId: number) {
+    this.orderService.finish(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Finished successfully');
+            this.order = data.data;
+            this.setStatuses();
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to finish the order');
+        }
+    );
+  }
+
+  cancelOrder(orderId: number) {
+    this.orderService.cancel(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Cancelled successfully');
+            this.order = data.data;
+            this.setStatuses();
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to cancel the order');
+        }
+    );
   }
 
 }
--- src/app/my-account/order/order.page.ts
+++ src/app/my-account/order/order.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from "../../model/order.model";
+import {configuration} from "../../model/configuration.model";
+import {OrderService} from "../../services/order.service";
+import {OrderCriteriaSearch} from "../../model/order-criteria-search.model";
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-order',
@@ -7,9 +12,176 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPage implements OnInit {
 
-  constructor() { }
+  orders:Array<Order> = new Array<Order>();
+  currentPage: number = 1;
+  totalPage: number;
+  pages: Array<number> = new Array<number>();
+
+  form: any = {};
+
+  private configuration = configuration;
+
+  constructor(private orderService: OrderService,
+              public alertController: AlertController) { }
 
   ngOnInit() {
+    this.form.status = 'all';
+    this.search(1);
+  }
+
+  async presentAlert(header: string, subHeader: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      subHeader: subHeader,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  isProcessing(order: Order): boolean {
+    if (order.status == 'PROCESSING') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isProcessed(order: Order): boolean {
+    if (order.status == 'PROCESSED') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isDelivering(order: Order): boolean {
+    if (order.status == 'DELIVERING') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFinished(order: Order): boolean {
+    if (order.status == 'FINISHED') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isCancelled(order: Order): boolean {
+    if (order.status == 'CANCELLED') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onSubmit() {
+    this.search(1);
+  }
+
+  search(page: number) {
+    let status = this.form.status;
+    if (status == 'all') {
+      status = null;
+    }
+    let ocs: OrderCriteriaSearch = new OrderCriteriaSearch(this.form.orderId, status, page, configuration.pageSize);
+    this.orderService.getOrders(ocs).subscribe(
+        data => {
+          this.orders = data.data;
+          this.currentPage = data.current;
+          this.totalPage = data.total;
+          this.makePages();
+        },
+        error => {
+          console.log(error);
+        }
+    );
+  }
+
+  makePages() {
+    this.pages = new Array<number>();
+    if (this.totalPage < 1) {
+      // do nothing
+    } else {
+      for (var i = 1; i <= this.totalPage; i++) {
+        this.pages.push(i);
+      }
+    }
+  }
+
+  gotoPage(page: number) {
+    if(page <1) {
+      page = 1;
+    }
+    this.search(page);
+  }
+
+  processOrder(orderId: number, index: number) {
+    this.orderService.process(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Processed successfully');
+            this.orders[index] = data.data;
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to process the order');
+        }
+    );
+  }
+
+  deliverOrder(orderId: number, index: number) {
+    this.orderService.deliver(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Delivering successfully');
+            this.orders[index] = data.data;
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to deliver the order');
+        }
+    );
+  }
+
+  finishOrder(orderId: number, index: number) {
+    this.orderService.finish(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Finished successfully');
+            this.orders[index] = data.data;
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to finish the order');
+        }
+    );
+  }
+
+  cancelOrder(orderId: number, index: number) {
+    this.orderService.cancel(orderId).subscribe(
+        data => {
+          if (data.success) {
+            this.presentAlert("Success", '', 'Cancelled successfully');
+            this.orders[index] = data.data;
+          } else {
+            this.presentAlert("Error", '', data.message);
+          }
+        },
+        error => {
+          this.presentAlert("Error", '', 'Failed to cancel the order');
+        }
+    );
   }
 
 }
